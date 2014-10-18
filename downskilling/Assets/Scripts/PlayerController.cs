19863178a56cee97cfa31b00900d0fe140c35ac2
Ben Schiffler <Ben.Schiffler@gmail.com>
2014-10-17 20:31:26 -0700

    Implemented basic player control, YO! DAWG! YO!

---
+++ downskilling/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	/*Movement*/
+	public float maxSpeed;
+	public float acceleration;
+	public float jumpSpeed;
+	public float translation;
+	
+	//Controlls
+	public string horizontalControl;
+	public string actionButton;
+	
+	
+	// Update is called once per frame
+	void Update () {
+		//moving horizontally
+		translation = Input.GetAxis (horizontalControl) * acceleration;
+		translation *= Time.deltaTime;
+		if (rigidbody2D.velocity.x < maxSpeed)
+			rigidbody2D.AddForce(new Vector2 (translation, 0));
+		
+	}
+}
